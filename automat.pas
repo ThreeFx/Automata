5e5b72bb96f245d53e24fc0e801a158b7536b976
Jakob Kress <jakobkress@hotmail.com>
2015-09-15 14:53:25 +0200

    hi

--- automat.pas
+++ automat.pas
@@ -1,6 +1,18 @@
 UNIT Automat;
 
+USES fgl;
+
 INTERFACE
+
+TYPE TZustand = CLASS
+	PRIVATE
+		next: TFPGMap; {TFPGMap --> Dicitonary --> generisches Array}
+	PUBLIC
+		CONSTRUCTOR Create(dict: TFPGMap);
+		DESTRUCTOR Dispose;
+		FUNCTION GetNext(zeichen: char): TZustand;
+	 	
+	
 
 IMPLEMENTATION
 
