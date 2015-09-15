d6b3ba096a90a6f27a253b4bc10a99b7b76025a2
Jakob Kress <jakobkress@hotmail.com>
2015-09-15 15:24:30 +0200

    cool

--- automat.pas
+++ automat.pas
@@ -1,20 +1,46 @@
 UNIT Automat;
 
-USES fgl;
-
 INTERFACE
 
-TYPE TZustand = CLASS
+USES fgl;
+
+TYPE 	
+	TZustand = CLASS;
+	TZustand = CLASS
 	PRIVATE
-		next: TFPGMap; {TFPGMap --> Dicitonary --> generisches Array}
+		next: TFPGMap<char,TZustand>; {TFPGMap --> Dicitonary --> generisches Array}
 	PUBLIC
-		CONSTRUCTOR Create(dict: TFPGMap);
+		CONSTRUCTOR Create(dict: TFPGMap<char,TZustand>);
 		DESTRUCTOR Dispose;
 		FUNCTION GetNext(zeichen: char): TZustand;
-	 	
+	END;
 	
 
 IMPLEMENTATION
+
+CONSTRUCTOR TZustand.Create(dict: TFPGMap<char,TZustand>);
+BEGIN
+	next := dict;
+END;
+
+DESTRUCTOR TZustand.Dispose;
+VAR
+	i: integer;
+BEGIN
+	FOR i:=0 TO next.Count-1 DO
+	BEGIN
+		next.Remove(next.Keys[i]);
+	END;
+END;
+
+FUNCTION TZustand.GetNext(zeichen: char): TZustand;
+VAR
+	sinnlos: integer;
+BEGIN
+	next.Find(zeichen,sinnlos);
+	result:=next.GetData(sinnlos);
+END;
+
 
 BEGIN
 END.
